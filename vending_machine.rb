47f4ed1ea1b6cdd069b747a85fce9b8f2cd507d1
Pratik <pratikc@shriffle.com>
2022-07-27 19:13:51 +0530

    2nd day work of demo project

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,23 +1,73 @@
-puts ""
-puts "===================================================================".center(144)
-puts "!! Welcome to our Vending Machine !!".center(144)
-puts "===================================================================".center(144)
+require_relative 'menu_work'
+include MenuWork
 
-def menu
-menu = {
-  "101":'Coca Cola',
-  "102":'Sprite',
-  "103":'Pepsi',
-  "104":'Fanta',
-  "105":'Lays'
-}
-menu.each { |key, value| puts "\t#{key}: #{value}" }
-puts "\tPress 0 = Quit."
+#Welcome messages prints here
+def welcome_screen
+  puts ""
+  puts "===================================================================".center(144)
+  puts "!! Welcome to our Vending Machine !!".center(144)
+  puts "===================================================================".center(144) 
 end
 
-menu
+public
+def price_by_selection(selection)
+  case selection
+    when 1
+      return 20
+    when 2
+      return 20
+    when 3
+      return 10
+    when 4
+      return 10
+    when 5
+      return 5
+  end 
+end
 
+def get_quantity(selection)
+  puts "How much?(maximum: 5)"
+  quantity = gets.chomp.to_i
 
-# puts menu.keys
-# puts menu.values
+  if quantity <= 5
+    price = price_by_selection(selection)
+    max_amount = quantity * price
+    puts "Total Rupees You Pay: #{max_amount}"
+  else
+    puts "Please enter valid quantity."
+  end
+end
+
+def cancel_method
+  puts "Enter 1 to continue or 2 to cancel."
+  cancel = gets.chomp.to_i
+
+  if cancel.eql? 1
+    get_quantity(selection)
+  elsif cancel.eql? 2
+    puts "Your order cancelled"
+    puts "========= Thank You! ========="
+    exit!
+  else
+    puts "Please enter valid input..."
+    cancel_method
+  end
+end
+
+class VendingMachine                         #Main Class Vending Machine
+  def self.clear
+    print "\e[2J\e[f"
+  end
+
+  def start
+    welcome_screen
+    selection = menus        #Module Method
+    VendingMachine.clear
+    menu_choosen(selection)  #Module Method
+    cancel_method
+  end
+end
+
+start_vending_machine = VendingMachine.new   #VendingMachine class object
+start_vending_machine.start                  ##All methods Call Here
 
